8c949fe535386429135ff75c611998558b82a67d
CJ <darkpimba01@gmail.com>
2024-11-21 15:36:10 -0300

    alter: utils.mjs

--- classes/utils.mjs
+++ classes/utils.mjs
@@ -1,3 +1,6 @@
+import fs from "fs";
+import { deserialize } from "v8";
+
 export default class Util {
 	constructor() {}
 
@@ -9,5 +12,24 @@ export default class Util {
 	/** Função que gera um número aleatório entre 0.0 e 100.0 */
 	gerarNumeroAleatorio0a100() {
 		return Math.random() * 100;
+	}
+
+	/** retorna os valores em formato de array
+	 * @returns {valores []}
+	 */
+	getValores(caminhoArquivo = "./valores.bin") {
+		try {
+			// Ler o arquivo binário
+			const binario = fs.readFileSync(caminhoArquivo);
+
+			// Desserializar o binário para obter o array
+			const array = deserialize(binario);
+
+			// Retornar o array
+			return array;
+		} catch (erro) {
+			console.error("Erro ao carregar ou processar os dados binários:", erro);
+			throw erro;
+		}
 	}
 }
--- index.mjs
+++ index.mjs
@@ -1,2 +1,7 @@
-import Treinamento from "./classes/treinamento.mjs";
-const treinamento = new Treinamento();
+import Individuo from "./classes/individuo.mjs";
+
+// treinamento vai continuar até o saldo de algum indivíduo chegar a mil.
+
+// o treinamento vai consistir em 100 indivíduos. O melhor vai ser o que demorar mais para perder todo o saldo.
+
+// os indivíduos que ficaram mais de 20 rodadas sem apostar teram o saldo zerado altomaticamente.
